8f9ceaaeed013a54c12c35f431a1c97585c07ea4
adeebbayat <adeebbayat@yahoo.com>
2024-01-02 11:09:12 -0800

    initial commit

--- algos/findcommon.js
+++ algos/findcommon.js
@@ -3,14 +3,20 @@ let array2 = [1,3,5,7,9]
 let array3 = [1,5,9,13,17]
 let combinedArray = [array1,array2,array3]
 
-function findCommon(array){
-  const my_set = new Set();
-  array.forEach((interiorArray) => {
-    interiorArray.forEach((element) => {
-        my_set.add(element)
-    })
-  })
-  return my_set
+function findCommonElements(arrays) {
+  if (arrays.length === 0) {
+    return []; // If there are no arrays, there are no common elements
+  }
+
+  // Use the first array as a reference
+  const referenceArray = arrays[0];
+
+  // Filter elements that are present in all arrays
+  const commonElements = referenceArray.filter((element) =>
+    arrays.every((array) => array.includes(element))
+  );
+
+  return commonElements;
 }
 
-console.log(findCommon(combinedArray))
+console.log(findCommonElements(combinedArray))
--- algos/test.js
+++ algos/test.js
@@ -1,18 +1,16 @@
-// ADD CODE HERE
-const groupBy = (array, callback) => {
-  const new_obj = {}
-  array.forEach((element)=>{
-    if(new_obj[callback(element)]){
-    let values = Object.values(new_obj[callback(element)])
-    values.push(element)
-    new_obj[callback(element)] = values}
-    else{
-      new_obj[callback(element)] = [element]
-    }
-  })
-  return new_obj
+const user1 = {
+  name: "Will",
+  score: 3,
+  increment: function(){
+    this.score++;
+  }
 }
-// Uncomment these to check your work!
-const decimals = [1.3, 2.1, 2.4];
-const floored = function(num) { return Math.floor(num); };
-console.log(groupBy(decimals, floored)); // should log: { 1: [1.3], 2: [2.1, 2.4] }
+
+user1.increment();
+user1.increment();
+user1.increment();
+user1.increment();
+user1.increment();
+user1.increment();
+user1.increment();
+console.log(user1)
